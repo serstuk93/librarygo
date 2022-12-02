e8f78eab34f2e182c7caa62762f04dc2532a08dc
serstuk <radoslav993@gmail.com>
2022-12-02 12:56:57 +0100

    getting API response for preselected book title

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+/*
+1. Create simple cli application for finding all works from authors of specific book
+2. Application has to find all authors for book and it will print list of all their works
+3. Create list of works for each author (name, revision)
+4. Print result to stdout in yaml format sorted by author name, count of revision (asc, desc as argument).
+Names of authors have to be part of output.
+*/
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	//"encoding/json"
+)
+
+type KeysSearch struct {
+	Key          string   `json:"key"`
+	AuthorsKeys  []string `json:"author_key"`
+	AuthorsNames []string `json:"author_name"`
+}
+
+type Docs struct {
+	Results []KeysSearch `json:"docs"`
+}
+
+type ExactMatch struct {
+	ExctMtch bool `json:"numFoundExact"`
+}
+
+func main() {
+	var introCli []string = []string{"This is LibraryGo created by Radoslav Serstuk.",
+		"For help type '-help'",
+		"To search authors of the book type '-search <name of the book>'"}
+	for _, element := range introCli {
+		fmt.Printf("%s  \n", element)
+	}
+	response, err := http.Get("https://openlibrary.org/search.json?q=has_fulltext:true%20AND%20title:the%20lord%20of%20the%20rings&fields=key,author_key,author_name,availability&limit=1")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+
+	var worksSearch Docs
+	if err := json.Unmarshal(responseData, &worksSearch); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("WORK ID", worksSearch.Results[0].Key)
+	for i, element := range worksSearch.Results[0].AuthorsKeys {
+		fmt.Print("namekey ", element)
+		fmt.Println(" name ", worksSearch.Results[0].AuthorsNames[i])
+	}
+
+}
